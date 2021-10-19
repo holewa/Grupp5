fb5f4172c6c1bc6ee1d7259556265025629656e5
JavaByMohamed <mohamed_abdel.monem@hotmail.se>
2021-10-19 13:09:44 +0200

    Defined helper methods in bookrepo, defined business logic in bookservice. Mobprogrammerat med hannes, Ivy, Joel och Mohamed

--- src/main/java/com/groupfive/bookmanager/repo/BookRepo.java
+++ src/main/java/com/groupfive/bookmanager/repo/BookRepo.java
@@ -4,9 +4,26 @@ import com.groupfive.bookmanager.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface BookRepo extends JpaRepository<Book, Integer> {
+import java.util.Optional;
 
+@Repository
+public interface BookRepo extends JpaRepository<Book, Long> {
+
+    Optional<Book> findByIsbn(String isbn);
+
+    Optional<Book> findByAuthor(String author);
+
+    Optional<Book> findByTitle(String title);
+
+    Optional<Book> findByGenre(String title);
+
+    Optional<Book> findByPublishedYear(Integer publishedYear);
+
+    Optional<Book> findByRating(Integer rating);
+
+    Optional<Book> deleteByIsbn(String isbn);
+
+    Optional<Book> deleteByTitle(String title);
 
 
 }
--- src/main/java/com/groupfive/bookmanager/service/BookService.java
+++ src/main/java/com/groupfive/bookmanager/service/BookService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BookService {
@@ -20,8 +21,8 @@ public class BookService {
 
         //separera felhantering(till egen klass) och själva metoden senare
         try {
-            List<Book> books = new ArrayList<Book>();
-            bookRepo.findAll().forEach(book -> books.add((Book) book));
+            List<Book> books = new ArrayList<>();
+            bookRepo.findAll().forEach(book -> books.add(book));
             if (books.isEmpty()) {
                 return new ResponseEntity<>(HttpStatus.NO_CONTENT);
             }
@@ -31,4 +32,87 @@ public class BookService {
         }
     }
 
+    public ResponseEntity<Optional<Book>> findBookByIsbn(String isbn) {
+        Optional<Book> books = bookRepo.findByIsbn(isbn);
+
+        return new ResponseEntity<>(books, HttpStatus.OK);
+    }
+
+    public ResponseEntity<Optional<Book>> findBookByTitle(String title) {
+        Optional<Book> books = bookRepo.findByTitle(title);
+        return new ResponseEntity<>(books, HttpStatus.OK);
+    }
+
+    public ResponseEntity<Optional<Book>> findBookByAuthor(String author) {
+        Optional<Book> books = bookRepo.findByAuthor(author);
+        return new ResponseEntity<>(books, HttpStatus.OK);
+    }
+
+    public ResponseEntity<Optional<Book>> findBookByGenre(String genre) {
+        Optional<Book> books = bookRepo.findByGenre(genre);
+        return new ResponseEntity<>(books, HttpStatus.OK);
+    }
+
+    public ResponseEntity<Optional<Book>> findBookByPublishedYear(Integer publishedYear) {
+        Optional<Book> books = bookRepo.findByPublishedYear(publishedYear);
+        return new ResponseEntity<>(books, HttpStatus.OK);
+    }
+
+    public ResponseEntity<Optional<Book>> findBookByRating(Integer rating) {
+        Optional<Book> books = bookRepo.findByRating(rating);
+        return new ResponseEntity<>(books, HttpStatus.OK);
+    }
+
+    public ResponseEntity<Optional<Book>> findBookById(Long id) {
+        Optional<Book> books = bookRepo.findById(id);
+        return new ResponseEntity<>(books, HttpStatus.OK);
+    }
+
+    public ResponseEntity<Book> createBook(Book book) {
+        Book newBook = bookRepo.save(book);
+        return new ResponseEntity<>(newBook, HttpStatus.CREATED);
+    }
+
+    public ResponseEntity<Book> updateBook(Book book, Long id) {
+        Optional<Book> bookData = bookRepo.findById(id);
+        if (bookData.isPresent()) {
+            Book bookUpdate = bookData.get();
+            if (book.getAuthor() != null) {
+                bookUpdate.setAuthor(book.getAuthor());
+            }
+            if (book.getGenre() != null) {
+                bookUpdate.setGenre(book.getGenre());
+            }
+            if (book.getIsbn() != null) {
+                bookUpdate.setIsbn(book.getIsbn());
+            }
+            if (book.getTitle() != null) {
+                bookUpdate.setTitle(book.getTitle());
+            }
+            if (book.getRating() != null) {
+                bookUpdate.setRating(book.getRating());
+            }
+            if (book.getPublishYear() != null) {
+                bookUpdate.setPublishYear(book.getPublishYear());
+            }
+            return new ResponseEntity<>(bookRepo.save(bookUpdate), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    public ResponseEntity<Book> deleteBookById(Long id) {
+        bookRepo.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    public ResponseEntity<Book> deleteBookByIsbn(String isbn) {
+        bookRepo.deleteByIsbn(isbn);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    public ResponseEntity<Book> deleteBookByTitle(String title) {
+        bookRepo.deleteByTitle(title);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
